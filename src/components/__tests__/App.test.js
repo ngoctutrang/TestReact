996c5ca69f81f4bf58aa5335656c4d5415aa3807
Ngoc Tu <tdngoctu@gmail.com>
2019-12-04 11:51:14 +0700

    checks App contain CommenetList

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -3,7 +3,15 @@ import ReactDom from 'react-dom';
 import App from '../App';
 import { shallow } from 'enzyme';
 import CommentBox from '../CommentBox';
+import ComponentList from '../CommentList';
+
+
 it('shows a comment box', () => {
 	const wrapped = shallow(<App />);
 	expect(wrapped.find(CommentBox).length).toEqual(1);
+});
+
+it('shows a ComponentList', () => {
+	const wrapped = shallow(<App />);
+	expect(wrapped.find(ComponentList).length).toEqual(1);
 })

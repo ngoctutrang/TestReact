b990081efe95bc692119122d4db916bc67badcfc
Do Ngoc Tu <tdngoctu@gmail.com>
2019-12-05 03:47:20 +0700

    add component Next

---
+++ src/components/ComponentNex.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ComponentNext extends Component {
+    render() {
+        return (
+            <div>
+                ComponentNext
+            </div>
+        )
+    }
+}

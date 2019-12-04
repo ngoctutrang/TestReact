cb14c29678094a2724c79de8687a28360ae84048
Do Ngoc Tu <tdngoctu@gmail.com>
2019-12-04 21:01:52 +0700

    add new line

---
+++ src/components/ComponentTask.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ComponentTask extends Component {
+    render() {
+        return (
+            <div>
+                Task- add new line
+            </div>
+        )
+    }
+}

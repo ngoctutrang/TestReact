a05f6bb743aa3599996e6b98ab70a89991212fe9
Ngoc Tu <tdngoctu@gmail.com>
2019-12-05 11:54:33 +0700

    Add test for CommentBox

---
+++ src/components/__tests__/CommentBox.test.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { mount } from 'enzyme';
+import CommentBox from 'components/CommentBox';
+
+it('has a text area and a button', () => {
+    const wrapped = mount(<CommentBox />);
+
+    expect(wrapped.find('textarea').length).toEqual(1);
+    expect(wrapped.find('button').length).toEqual(1);
+})

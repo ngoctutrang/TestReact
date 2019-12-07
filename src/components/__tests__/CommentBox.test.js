a994add96b59baadc4cb91ecd582ae7aa76f648b
Ngoc Tu <tdngoctu@gmail.com>
2019-12-07 15:23:21 +0700

    simulate on CommentBox component

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -15,4 +15,19 @@ afterEach(() => {
 it('has a text area and a button', () => {
     expect(wrapped.find('textarea').length).toEqual(1);
     expect(wrapped.find('button').length).toEqual(1);
+});
+
+it('has a texterea that user can type in', () => {
+    //event: change
+    //mock: event => event.target.value
+    wrapped.find('textarea').simulate('change', {
+        target:{
+            value: 'New Comment'
+        }
+    });
+
+    // Force component update
+    wrapped.update();
+
+    expect(wrapped.find('textarea').prop('value')).toEqual('New Comment');
 })
